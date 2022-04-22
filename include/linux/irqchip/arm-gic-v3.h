153a986653395135f41a5fa3c27b9719697d6587
TheKit <nekit1000@gmail.com>
2022-04-22 17:03:24 +0300

    irqchip/gic-v3: fix compilation with KVM enabled due to MTK changes

--- include/linux/irqchip/arm-gic-v3.h
+++ include/linux/irqchip/arm-gic-v3.h
@@ -371,6 +371,15 @@
 #define ICH_LR_PHYS_ID_MASK		(0x3ffULL << ICH_LR_PHYS_ID_SHIFT)
 #define ICH_LR_PRIORITY_SHIFT		48
 
+/* These are for GICv2 emulation only */
+#ifndef GICH_LR_VIRTUALID
+#define GICH_LR_VIRTUALID		(0x3ffUL << 0)
+#endif
+#define GICH_LR_PHYSID_CPUID_SHIFT	(10)
+#ifndef GICH_LR_PHYSID_CPUID
+#define GICH_LR_PHYSID_CPUID		(7UL << GICH_LR_PHYSID_CPUID_SHIFT)
+#endif
+
 #define ICH_MISR_EOI			(1 << 0)
 #define ICH_MISR_U			(1 << 1)
 
